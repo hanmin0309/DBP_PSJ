4bcbcb8d195eab8469787a7dbda9f30ba589f20e
김한민 <kdg5794@gmail.com>
2025-11-22 11:26:09 +0900

    초기 로그인1차화면 25_11_22

---
+++ WindowsFormsApp5/login.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class login
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            this.airForm1 = new ReaLTaiizor.Forms.AirForm();
+            this.notice_label = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.crownLabel1 = new ReaLTaiizor.Controls.CrownLabel();
+            this.passWord_Box = new System.Windows.Forms.RichTextBox();
+            this.iD_Box = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notice_label2 = new System.Windows.Forms.Label();
+            this.airForm1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // airForm1
+            // 
+            this.airForm1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(58)))), ((int)(((byte)(65)))));
+            this.airForm1.BorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.airForm1.Controls.Add(this.notice_label2);
+            this.airForm1.Controls.Add(this.notice_label);
+            this.airForm1.Controls.Add(this.pictureBox1);
+            this.airForm1.Controls.Add(this.button1);
+            this.airForm1.Controls.Add(this.login_btn);
+            this.airForm1.Controls.Add(this.crownLabel1);
+            this.airForm1.Controls.Add(this.passWord_Box);
+            this.airForm1.Controls.Add(this.iD_Box);
+            this.airForm1.Controls.Add(this.label4);
+            this.airForm1.Controls.Add(this.label3);
+            this.airForm1.Controls.Add(this.label2);
+            this.airForm1.Controls.Add(this.label1);
+            this.airForm1.Customization = "AAAA/1paWv9ycnL/";
+            this.airForm1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.airForm1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.airForm1.Image = null;
+            this.airForm1.Location = new System.Drawing.Point(0, 0);
+            this.airForm1.MinimumSize = new System.Drawing.Size(112, 35);
+            this.airForm1.Movable = true;
+            this.airForm1.Name = "airForm1";
+            this.airForm1.NoRounding = false;
+            this.airForm1.Sizable = true;
+            this.airForm1.Size = new System.Drawing.Size(800, 450);
+            this.airForm1.SmartBounds = true;
+            this.airForm1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.airForm1.TabIndex = 0;
+            this.airForm1.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.airForm1.Transparent = false;
+            this.airForm1.Click += new System.EventHandler(this.airForm1_Click);
+            // 
+            // notice_label
+            // 
+            this.notice_label.AutoSize = true;
+            this.notice_label.ForeColor = System.Drawing.Color.White;
+            this.notice_label.Location = new System.Drawing.Point(46, 306);
+            this.notice_label.Name = "notice_label";
+            this.notice_label.Size = new System.Drawing.Size(13, 20);
+            this.notice_label.TabIndex = 10;
+            this.notice_label.Text = " ";
+            this.notice_label.Click += new System.EventHandler(this.notice_label_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(555, 149);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(163, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(150)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(167, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 30);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "가입하기";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // login_btn
+            // 
+            this.login_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(101)))), ((int)(((byte)(242)))));
+            this.login_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.login_btn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(101)))), ((int)(((byte)(242)))));
+            this.login_btn.FlatAppearance.BorderSize = 0;
+            this.login_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login_btn.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_btn.ForeColor = System.Drawing.Color.Transparent;
+            this.login_btn.Location = new System.Drawing.Point(38, 354);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(415, 55);
+            this.login_btn.TabIndex = 7;
+            this.login_btn.Text = "로그인";
+            this.login_btn.UseVisualStyleBackColor = false;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // crownLabel1
+            // 
+            this.crownLabel1.AutoSize = true;
+            this.crownLabel1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.crownLabel1.ForeColor = System.Drawing.Color.LightGray;
+            this.crownLabel1.Location = new System.Drawing.Point(12, 422);
+            this.crownLabel1.Name = "crownLabel1";
+            this.crownLabel1.Size = new System.Drawing.Size(159, 23);
+            this.crownLabel1.TabIndex = 6;
+            this.crownLabel1.Text = "계정이 필요한가요?";
+            this.crownLabel1.Click += new System.EventHandler(this.crownLabel1_Click);
+            // 
+            // passWord_Box
+            // 
+            this.passWord_Box.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.passWord_Box.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passWord_Box.ForeColor = System.Drawing.SystemColors.Window;
+            this.passWord_Box.Location = new System.Drawing.Point(38, 248);
+            this.passWord_Box.Name = "passWord_Box";
+            this.passWord_Box.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.passWord_Box.Size = new System.Drawing.Size(415, 55);
+            this.passWord_Box.TabIndex = 5;
+            this.passWord_Box.Text = "";
+            this.passWord_Box.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged_1);
+            // 
+            // iD_Box
+            // 
+            this.iD_Box.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.iD_Box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.iD_Box.ForeColor = System.Drawing.SystemColors.Window;
+            this.iD_Box.Location = new System.Drawing.Point(38, 149);
+            this.iD_Box.Name = "iD_Box";
+            this.iD_Box.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.iD_Box.Size = new System.Drawing.Size(415, 55);
+            this.iD_Box.TabIndex = 4;
+            this.iD_Box.Text = "";
+            this.iD_Box.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(33, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "비밀번호";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(33, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "아이디";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(138, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "다시 돌아왔구나, 오태식이!";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(92, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "오셨군요? 환영합니다!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // notice_label2
+            // 
+            this.notice_label2.AutoSize = true;
+            this.notice_label2.ForeColor = System.Drawing.Color.White;
+            this.notice_label2.Location = new System.Drawing.Point(46, 306);
+            this.notice_label2.Name = "notice_label2";
+            this.notice_label2.Size = new System.Drawing.Size(13, 20);
+            this.notice_label2.TabIndex = 11;
+            this.notice_label2.Text = " ";
+            this.notice_label2.Click += new System.EventHandler(this.notice_label2_Click);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(58)))), ((int)(((byte)(65)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.airForm1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(112, 35);
+            this.Name = "login";
+            this.ShowIcon = false;
+            this.Text = "themeForm1";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Load += new System.EventHandler(this.login_Load);
+            this.airForm1.ResumeLayout(false);
+            this.airForm1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Forms.AirForm airForm1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox passWord_Box;
+        private System.Windows.Forms.RichTextBox iD_Box;
+        private ReaLTaiizor.Controls.CrownLabel crownLabel1;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label notice_label;
+        private System.Windows.Forms.Label notice_label2;
+    }
+}
+
---
+++ WindowsFormsApp5/login.cs
@@ -0,0 +1,326 @@
+﻿// NuGet에서 MySql.Data 설치 필요
+using MySql.Data.MySqlClient;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class login : Form
+    {
+        // ▶ MySQL 연결 문자열 (본인 환경에 맞게 수정 필수)
+        private string _connectionString =
+            "server=223.130.151.111;port=3306;database=s5701514;uid=s5701514;pwd=s5701514;";
+
+        public login()
+        {
+            InitializeComponent();
+
+            // 폼 생성 시, 각 텍스트 박스의 SizeChanged 이벤트에
+            // 폰트 크기 조정 함수(textBox_SizeChanged)를 연결
+            if (this.Controls.Contains(iD_Box))
+            {
+                iD_Box.SizeChanged += textBox_SizeChanged;
+            }
+            if (this.Controls.Contains(passWord_Box))
+            {
+                passWord_Box.SizeChanged += textBox_SizeChanged;
+            }
+        }
+
+        // 폼 로드 이벤트 이름을 login_Load 로 맞추는 것을 권장
+        private void login_Load(object sender, EventArgs e)
+        {
+            // 폼 로드 시 초기 폰트 크기 설정
+            textBox_SizeChanged(iD_Box, EventArgs.Empty);
+            textBox_SizeChanged(passWord_Box, EventArgs.Empty);
+
+            // 로그인 버튼 모서리 둥글게
+            RoundButtonCorners(login_btn, 8);
+
+            // 알림 라벨 초기화
+            notice_label.Text = "";
+            notice_label.ForeColor = Color.Red;
+            notice_label.Visible = false;   // 처음에는 숨김
+
+            notice_label2.Text = "";
+            notice_label2.ForeColor = Color.Red;
+            notice_label2.Visible = false;  // 처음에는 숨김
+        }
+
+        private void airRadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void airForm2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Control control = sender as Control;
+            if (control == null) return;
+
+            int newFontSize = (int)(control.Height * 0.55);
+
+            const int MIN_FONT_SIZE = 8;
+            const int MAX_FONT_SIZE = 30;
+
+            if (newFontSize < MIN_FONT_SIZE)
+                newFontSize = MIN_FONT_SIZE;
+            if (newFontSize > MAX_FONT_SIZE)
+                newFontSize = MAX_FONT_SIZE;
+
+            try
+            {
+                control.Font = new Font(control.Font.FontFamily, newFontSize, control.Font.Style);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"폰트 설정 오류: {ex.Message}");
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            Control control = sender as Control;
+            if (control == null) return;
+
+            int newFontSize = (int)(control.Height * 0.55);
+
+            const int MIN_FONT_SIZE = 8;
+            const int MAX_FONT_SIZE = 18;
+
+            if (newFontSize < MIN_FONT_SIZE)
+                newFontSize = MIN_FONT_SIZE;
+            if (newFontSize > MAX_FONT_SIZE)
+                newFontSize = MAX_FONT_SIZE;
+
+            try
+            {
+                control.Font = new Font(control.Font.FontFamily, newFontSize, control.Font.Style);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"폰트 설정 오류: {ex.Message}");
+            }
+        }
+
+        private void textBox_SizeChanged(object sender, EventArgs e)
+        {
+            Control control = sender as Control;
+            if (control == null) return;
+
+            int newFontSize = (int)(control.Height * 0.55);
+
+            const int MIN_FONT_SIZE = 8;
+            const int MAX_FONT_SIZE = 18;
+
+            if (newFontSize < MIN_FONT_SIZE)
+                newFontSize = MIN_FONT_SIZE;
+            if (newFontSize > MAX_FONT_SIZE)
+                newFontSize = MAX_FONT_SIZE;
+
+            try
+            {
+                control.Font = new Font(control.Font.FontFamily, newFontSize, control.Font.Style);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"폰트 설정 오류: {ex.Message}");
+            }
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void airForm1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 기존 테스트용 버튼 (있다면)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RoundButtonCorners(login_btn, 8);
+        }
+
+        private void crownLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // ✅ 로그인 버튼 클릭 이벤트 (핵심)
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            // 1. ID / PW 읽기
+            string userId = iD_Box.Text.Trim();
+            string userPw = passWord_Box.Text.Trim();
+
+            // 2. 빈 값 체크
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(userPw))
+            {
+                notice_label.ForeColor = Color.White;
+                notice_label2.ForeColor = Color.White;
+
+                // 여기서는 예시로 notice_label만 사용
+                notice_label.Text = "ID와 PW를 입력하세요";
+                notice_label.Visible = true;
+
+                notice_label2.Text = "";
+                notice_label2.Visible = false;
+
+                return;
+            }
+
+            // 3. MySQL에서 계정 존재 여부 확인
+            bool isExist = CheckUserExists(userId, userPw);
+
+            if (!isExist)
+            {
+                // ❌ 계정이 없을 때
+                notice_label.ForeColor = Color.White;
+                notice_label2.ForeColor = Color.White;
+
+                // 예: 여기서는 notice_label2를 사용하고, 1은 숨김
+                notice_label.Text = "";
+                notice_label.Visible = false;
+
+                notice_label2.Text = "계정을 생성해주세요.";
+                notice_label2.Visible = true;
+            }
+            else
+            {
+                // ✅ 계정이 있을 때 (로그인 성공)
+                notice_label.ForeColor = Color.White;
+                notice_label2.ForeColor = Color.White;
+
+                // 예: 로그인 성공은 notice_label에 출력
+                notice_label.Text = "로그인 성공!";
+                notice_label.Visible = true;
+
+                notice_label2.Text = "";
+                notice_label2.Visible = false;
+
+                // TODO: 로그인 성공 후 동작 (원하면 나중에 추가)
+                // MainForm main = new MainForm();
+                // main.Show();
+                // this.Hide();
+            }
+        }
+
+        private void RoundButtonCorners(Button btn, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            Rectangle rect = new Rectangle(0, 0, btn.Width, btn.Height);
+
+            int r = radius;
+            path.AddArc(rect.X, rect.Y, r, r, 180, 90);                    // 좌상단
+            path.AddArc(rect.Right - r, rect.Y, r, r, 270, 90);            // 우상단
+            path.AddArc(rect.Right - r, rect.Bottom - r, r, r, 0, 90);     // 우하단
+            path.AddArc(rect.X, rect.Bottom - r, r, r, 90, 90);            // 좌하단
+            path.CloseAllFigures();
+
+            btn.Region = new Region(path);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notice_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notice_label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // =====================================
+        //   MySQL 계정 존재 여부 확인 함수
+        // =====================================
+        private bool CheckUserExists(string id, string pw)
+        {
+            // ⚠️ 테이블/컬럼 이름을 실제 DB에 맞게 수정해야 합니다.
+            // 예시: users 테이블, user_id / user_pw 컬럼 사용
+            string query = "SELECT COUNT(*) FROM UserInformation WHERE Identity = @id AND Password = @pw";
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@pw", pw);
+
+                    conn.Open();
+                    object result = cmd.ExecuteScalar();
+
+                    int count = 0;
+                    if (result != null && int.TryParse(result.ToString(), out count))
+                    {
+                        return count > 0;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                // 예외 발생 시 콘솔에 출력(개발용)
+                Console.WriteLine("MySQL 오류: " + ex.Message);
+                MessageBox.Show("MySQL 오류: " + ex.Message);
+
+                // 사용자에게는 서버 오류 메시지 표시
+                // 예: 서버 오류는 notice_label2에만 출력하고, 1은 숨김
+                notice_label.ForeColor = Color.Red;
+                notice_label2.ForeColor = Color.Red;
+
+                notice_label.Text = "";
+                notice_label.Visible = false;
+
+                notice_label2.Text = "서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.";
+                notice_label2.Visible = true;
+
+                return false;
+            }
+        }
+    }
+}
